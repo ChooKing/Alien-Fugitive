0ee9dfc480802a125d84c8632613ed2b764c6ad0
Choo King <choocodes@gmail.com>
2024-09-22 18:11:19 -0400

    Rename CONSTS to Globals.

--- consts.js
+++
@@ -1,5 +0,0 @@
-export const CONSTS = {
-    GAME_WIDTH: 1600,
-    GAME_HEIGHT: 900,
-    SKY_SIZE: 500
-}
---
+++ globals.js
@@ -0,0 +1,8 @@
+export const Globals = {
+    GAME_WIDTH: 1600,
+    GAME_HEIGHT: 900,
+    SKY_SIZE: 500,
+    PELLET_SIZE: 7,
+    UFOs: [],
+    Pellets: []
+}
