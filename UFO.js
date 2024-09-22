29a69196b09ae72930d0c26e56dae64f06d82d54
Choo King <choocodes@gmail.com>
2024-09-22 18:12:00 -0400

    Adjust range of UFOs.

--- UFO.js
+++ UFO.js
@@ -1,9 +1,9 @@
 import {Sprite} from "./Sprite.js";
-import {CONSTS} from "./consts.js";
+import {Globals} from "./globals.js";
 
 export class UFO extends Sprite {
     constructor(pos, speed) {
-        super(pos, "/sprites/ufo.png", 1600, 54, 10, {minX: -160, maxX: CONSTS.GAME_WIDTH + 160, minY: 0, maxY: CONSTS.GAME_HEIGHT});
+        super(pos, "/sprites/ufo.png", 1600, 54, 10, {minX: -160, maxX: Globals.GAME_WIDTH + 160, minY: 0, maxY: Globals.GAME_HEIGHT});
         this.speed.x = speed;
         this.fps = speed * 0.9;
     }
